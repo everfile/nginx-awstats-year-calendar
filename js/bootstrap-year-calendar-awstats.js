d28b5b376962821ec4cc639b43e97b80c6cc1473
evergist <evergist@users.noreply.github.com>
2017-04-13 10:08:00 +0800

    Update bootstrap-year-calendar-awstats.js

--- js/bootstrap-year-calendar-awstats.js
+++ js/bootstrap-year-calendar-awstats.js
@@ -276,7 +276,7 @@
 							cellContent.addClass('day-content');
 							cellContent.text(currentDate.getDate());
 							cellContent.attr({
-								"href" : (currentDate.getFullYear()).slice(-2) + ("0" + (currentDate.getMonth() + 1)).slice(-2) + ("0" + currentDate.getDate()).slice(-2) + "/html/awstats.default.html",
+								"href" : (currentDate.getFullYear().toString()).slice(-2) + ("0" + (currentDate.getMonth() + 1)).slice(-2) + ("0" + currentDate.getDate()).slice(-2) + "/html/awstats.default.html",
 								"target" : "_blank"
 							});
 							cell.append(cellContent);
